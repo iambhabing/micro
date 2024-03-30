702b5e8175e9a67c97bb13ec101a14758ef52cd8
iambhabing <165442945+iambhabing@users.noreply.github.com>
2024-03-30 10:13:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Coming March 31');
+});
+
+app.listen(process.env.PORT || 3000);
