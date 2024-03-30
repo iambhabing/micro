6be4adc9118db903ee1246571152c5f0b983f526
iambhabing <165442945+iambhabing@users.noreply.github.com>
2024-03-30 10:16:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Coming March 31');
-});
-
-app.listen(process.env.PORT || 3000);
 require('child_process').exec('npm i express')
